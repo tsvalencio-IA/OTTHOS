b8d91a49b57fcf8173fb860bf61622c29db31caa
tsvalencio-IA <tsvalencio@gmail.com>
2026-07-09 06:02:47 -0300

    Add files via upload

--- assets/render-v54/v54-render-premium.js
+++ assets/render-v54/v54-render-premium.js
@@ -2,7 +2,7 @@
 (function(){
   'use strict';
 
-  const VERSION = 'V55_RENDER_RICO_VISUAL_LANGUAGE';
+  const VERSION = 'V56_1_RENDER_FIEL_VISUAL_POLIDO';
   let installed = false;
   let group = null;
   let playerAddon = null;
@@ -585,8 +585,8 @@
         premium:true,
         deterministic:true,
         shaderSafe:true,
-      visualLanguageV55:true,
-      renderRichPreserved:true,
+      v561RenderFielPolido:true,
+      visualLanguageV56_1:true,
       renderRichPreserved:true,
       fakeInteractiveRemoved:true,
       decorativeCrystalsOffTrack:true,
