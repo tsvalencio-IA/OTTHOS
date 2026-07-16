eaf95e847b1c1a42f32c5c5ac8c295a8fab9ee88
tsvalencio-IA <tsvalencio@gmail.com>
2026-07-15 23:46:00 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,13 @@
-// Configuração pública do app Web Firebase.
-// Também pode ser colada dentro de Configurações > Multiplayer.
-// Nunca coloque Service Account ou chave privada no GitHub.
-window.OTTHOS_FIREBASE_CONFIG = window.OTTHOS_FIREBASE_CONFIG || {
-  enabled:false, room:"vila-do-sol", apiKey:"", authDomain:"", databaseURL:"",
-  projectId:"", storageBucket:"", messagingSenderId:"", appId:""
+// Configuração pública do Firebase do OTTHOS
+window.OTTHOS_FIREBASE_CONFIG = {
+  enabled: true,
+  room: "vila-do-sol",
+
+  apiKey: "AIzaSyAGr4SzZOUoNpkgcfh8D5Pv4StMkqAAX_0",
+  authDomain: "lerunnersmap.firebaseapp.com",
+  databaseURL: "https://lerunnersmap-default-rtdb.firebaseio.com",
+  projectId: "lerunnersmap",
+  storageBucket: "lerunnersmap.firebasestorage.app",
+  messagingSenderId: "913793840199",
+  appId: "1:913793840199:web:97a019373c45943ca6ed62"
 };
